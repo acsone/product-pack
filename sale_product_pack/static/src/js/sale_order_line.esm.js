cfaedabcbf3798d1e606139b42387c4cb2232992
Stéphane Mangin <stephane.mangin@webmel.com>
2026-02-26 11:22:12 +0100

    fixup! [18.0][IMP] sale_product_pack: improve ergonomy for the user experience

--- sale_product_pack/static/src/js/sale_order_line.esm.js
+++ sale_product_pack/static/src/js/sale_order_line.esm.js
@@ -1,15 +1,87 @@
 /* Copyright 2026 ACSONE SA/NV */
-import {AlertDialog} from "@web/core/confirmation_dialog/confirmation_dialog";
 import {_t} from "@web/core/l10n/translation";
+import {AlertDialog} from "@web/core/confirmation_dialog/confirmation_dialog";
 import {patch} from "@web/core/utils/patch";
 import {StaticList} from "@web/model/relational_model/static_list";
 
 patch(StaticList.prototype, {
+    _getRecordIdentifier(record) {
+        return record?.resId || record?.id || null;
+    },
+
+    _isSaleOrderLine(record) {
+        return Boolean(record && record.resModel === "sale.order.line");
+    },
+
+    _isPackChildRecord(record) {
+        return (
+            this._isSaleOrderLine(record) &&
+            Boolean(record.data && record.data.pack_parent_line_id)
+        );
+    },
+
+    _isNonModifiablePackParent(record) {
+        return (
+            this._isSaleOrderLine(record) &&
+            !(record.data && record.data.pack_parent_line_id) &&
+            !(record.data && record.data.pack_modifiable)
+        );
+    },
+
+    _getParentId(record) {
+        return record?.data?.pack_parent_line_id?.[0] || null;
+    },
+
+    _getPackChildRecords(parentRecord, records) {
+        const sourceRecords = Array.isArray(records) ? records : [];
+        const parentId = this._getRecordIdentifier(parentRecord);
+        if (!parentId) {
+            return [];
+        }
+        return sourceRecords.filter(
+            (record) =>
+                this._isPackChildRecord(record) &&
+                this._getParentId(record) === parentId
+        );
+    },
+
+    _expandRecordsToDelete(records) {
+        const selectedRecords = Array.isArray(records) ? records : [];
+        const currentRecords = Array.isArray(this.records) ? this.records : [];
+        const result = [...selectedRecords];
+        const seen = new Set(
+            selectedRecords.map((record) => this._getRecordIdentifier(record))
+        );
+
+        for (const record of selectedRecords) {
+            if (!this._isNonModifiablePackParent(record)) {
+                continue;
+            }
+            for (const childRecord of this._getPackChildRecords(
+                record,
+                currentRecords
+            )) {
+                const childId = this._getRecordIdentifier(childRecord);
+                if (!seen.has(childId)) {
+                    result.push(childRecord);
+                    seen.add(childId);
+                }
+            }
+        }
+        return result;
+    },
+
+    _isChildDeletedWithoutParent(record, selectedParentIds) {
+        return (
+            this._canBeDeleted(record) &&
+            !selectedParentIds.has(this._getParentId(record))
+        );
+    },
+
     _canBeDeleted(record) {
         return (
-            record.resModel === "sale.order.line" &&
-            record.data.pack_parent_line_id &&
-            !record.data.pack_modifiable
+            this._isPackChildRecord(record) &&
+            !(record.data && record.data.pack_modifiable)
         );
     },
 
@@ -28,41 +100,27 @@ patch(StaticList.prototype, {
         });
     },
 
-    _deleteChildRecord(record) {
-        // Find and delete all child lines that belong to this pack parent
-        const childLines = this.records.filter(
-            (r) =>
-                r.data.pack_parent_line_id &&
-                r.data.pack_parent_line_id[0] === record.resId
-        );
-        for (const childLine of childLines) {
-            super.delete.call(this, childLine);
-        }
-    },
-
-    _deleteChildRecords(records) {
-        for (const record of records) {
-            this._deleteChildRecord(record);
-        }
-    },
-
     async delete(record) {
-        if (this._canBeDeleted(record)) {
-            this._alertNotUnlinkable(false);
-            return;
-        }
-        /* If authorized anf if this record is a packed product line, also delete existing child lines */
-        this._deleteChildRecord(record);
-        return super.delete(...arguments);
+        return this.deleteRecords([record]);
     },
 
     async deleteRecords(records) {
-        if (records.some((record) => this._canBeDeleted(record))) {
-            this._alertNotUnlinkable(true);
+        if (!Array.isArray(records)) {
+            return super.deleteRecords(...arguments);
+        }
+        const recordsToDelete = this._expandRecordsToDelete(records);
+        const selectedParentIds = new Set(
+            recordsToDelete
+                .filter((record) => !this._isPackChildRecord(record))
+                .map((record) => this._getRecordIdentifier(record))
+        );
+        const hasProtectedChild = recordsToDelete.some((record) =>
+            this._isChildDeletedWithoutParent(record, selectedParentIds)
+        );
+        if (hasProtectedChild) {
+            this._alertNotUnlinkable(records.length > 1);
             return;
         }
-        /* If authorized and if some of these records are packed product lines, also delete existing child lines */
-        this._deleteChildRecords(records);
-        return super.deleteRecords(...arguments);
+        return super.deleteRecords(recordsToDelete);
     },
 });
